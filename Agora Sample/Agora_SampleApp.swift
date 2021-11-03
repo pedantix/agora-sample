05c9b118fb4b001aaddb626c4d3ede9e803d69ac
Shaun Hubbard <shaunhubbard2013@icloud.com>
2021-11-03 09:39:09 -0400

    Initial Commit

---
+++ Agora Sample/Agora_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agora_SampleApp.swift
+//  Agora Sample
+//
+//  Created by shaun on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Agora_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
